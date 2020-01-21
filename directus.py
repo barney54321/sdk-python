2a6c737cbce33eb9cf8e7057c897480fe5421d4a
Andrew Esteban <andrewesteban1999@gmail.com>
2020-01-21 13:49:57 +1100

    added get_files and create_item

--- directus.py
+++ directus.py
@@ -4,29 +4,39 @@ import requests, json
 
 class DirectusClient:
 
-    def __init__(self, url, project, email, password):
+    def __init__(self, url, project, email=None, password=None):
         self.url = url + '/' + project
         self.email = email
         self.password = password
         self.project = project
+        self.access = None
 
-        payload = {
-            "email": email,
-            "password": password
-        }
+        if email is not None and password is not None:
 
-        combined_url = self.url + '/auth/authenticate'
-        r = requests.post(combined_url, data=payload)
-  
-        # extracting data in json format 
-        data = r.json() 
+            payload = {
+                "email": email,
+                "password": password
+            }
 
-        ACCESS_TOKEN = data['data']["token"]
+            combined_url = self.url + '/auth/authenticate'
+            r = requests.post(combined_url, data=payload)
+            data = r.json() 
 
-    def get_items(self, collection, id=None):
-        # GET /:project/items/:collection
+            self.access = data['data']["token"]
 
-        if id is None:
-            return requests.get(self.url + "/items/" + collection).json()
-        else:
-            return requests.get(self.url + "/items/" + collection + "/" + str(id)).json()
+    def get_items_list(self, collection):
+        return requests.get(self.url + "/items/" + collection).json()
+
+    def get_items(self, collection, id):
+        return requests.get(self.url + "/items/" + collection + "/" + str(id)).json()
+
+    def create_item(self, collection, item):
+        return requests.post(self.url + "/items/" + collection, data=item)
+
+    def get_files_list(self):
+        headers = {"Authorization": "Bearer " + self.access}
+        return requests.get(self.url + "/files", headers=headers).json() 
+
+    def get_file(self, id):
+        headers = {"Authorization": "Bearer " + self.access}
+        return requests.get(self.url + "/files/" + str(id), headers=headers).json() 
